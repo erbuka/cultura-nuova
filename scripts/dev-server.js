7bc921156a1382e3749c6e40f40a61ff3e6247f4
Andrea Bucaletti <erbuka@hotmail.com>
2020-03-10 17:13:25 +0100

    -_

--- scripts/dev-server.js
+++ scripts/dev-server.js
@@ -4,8 +4,8 @@ const path = require("path");
 const app = express();
 const port = 8080;
 
-app.use("/assets", express.static("assets"), (req, res) => res.sendFile(path.resolve("./dev/index.html")));
-app.use("/", express.static("dev"));
+app.use("/assets", express.static("assets"), (req, res) => res.sendFile(path.resolve("./build-dev/index.html")));
+app.use("/", express.static("build-dev"));
 
 app.listen(port);
 
